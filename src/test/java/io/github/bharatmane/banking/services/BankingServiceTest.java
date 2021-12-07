5fb3ae0745a6c8f9d95695a6db299ff1f0b234b0
bharat.mane <bharat.mane@outlook.com>
2021-12-07 16:52:30 +0530

    feat: added support for transfer logic

--- src/test/java/io/github/bharatmane/banking/services/BankingServiceTest.java
+++ src/test/java/io/github/bharatmane/banking/services/BankingServiceTest.java
@@ -2,6 +2,7 @@ package io.github.bharatmane.banking.services;
 
 import io.github.bharatmane.banking.Prompter;
 import io.github.bharatmane.banking.entity.Customer;
+import io.github.bharatmane.banking.exception.InsufficientFundsException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,9 +33,7 @@ class BankingServiceTest {
                 new Customer("1235", "jill@123", "+919632104324", "200.2")
         ));
         customerService = new CustomerService(customers);
-
     }
-
 
     @Test
     @DisplayName("Should have valid list of customers when initialized")
@@ -52,8 +51,8 @@ class BankingServiceTest {
     }
 
     @Test
-    @DisplayName("Should show accounts menu when logged in with correct credentials")
-    void  shouldShowAccountsMenuWhenLoggedInWithCorrectCredentials() {
+    @DisplayName("Should be logged in with correct credentials")
+    void  shouldBeLoggedInWithCorrectCredentials() {
         Scanner scanner = new Scanner("1\n1234\njack@123\n1\n");
         prompter = new Prompter(printStream,scanner);
 
@@ -85,4 +84,60 @@ class BankingServiceTest {
         assertThat(outputStream.toString()).contains("Invalid user name or password");
     }
 
+    @Test
+    @DisplayName("Should be logged out when log out option chosen")
+    void  shouldBeLoggedOutWhenLogoutOptionChosen() throws InsufficientFundsException {
+        Scanner scanner = new Scanner("1\n1234\njack@123\n4\n");
+        prompter = new Prompter(printStream,scanner);
+
+        //Given
+        BankingService bankingService = new BankingService(customerService,prompter);
+
+        //When
+        bankingService.greet();
+        Customer customer = bankingService.login();
+        bankingService.operate(customer);
+
+        //Then
+        assertThat(customer.isLoggedIn()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should decrease balance of customer 1 when transferred to customer 2")
+    void  shouldDecreaseBalanceOfCustomer1WhenTransferredToCustomer2() throws InsufficientFundsException {
+        Scanner scanner = new Scanner("1\n1234\njack@123\n1\n100\n3\n1235\n100\n4");
+        prompter = new Prompter(printStream,scanner);
+
+        //Given
+        BankingService bankingService = new BankingService(customerService,prompter);
+
+        //When
+        bankingService.greet();
+        Customer customer = bankingService.login();
+        bankingService.operate(customer);
+
+        //Then
+        assertThat(customer.getAccountBalance()).isEqualTo("100.1");
+
+    }
+
+    @Test
+    @DisplayName("Should increase balance of customer 2 when transferred to customer 2")
+    void  shouldIncreaseBalanceOfCustomer2WhenTransferredToCustomer2() throws InsufficientFundsException {
+        Scanner scanner = new Scanner("1\n1234\njack@123\n1\n100\n3\n1235\n100\n4");
+        prompter = new Prompter(printStream,scanner);
+
+        //Given
+        BankingService bankingService = new BankingService(customerService,prompter);
+
+        //When
+        bankingService.greet();
+        Customer customer = bankingService.login();
+        bankingService.operate(customer);
+        Customer customer2 = bankingService.getCustomer("1235");
+
+        //Then
+        assertThat(customer2.getAccountBalance()).isEqualTo("300.2");
+
+    }
 }
